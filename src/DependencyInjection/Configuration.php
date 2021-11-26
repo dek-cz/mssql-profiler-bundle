de6a63e615d9bbaca7ecca9fd86edb9f97a5a6c1
Záruba Jan <jan.zaruba@dek-cz.com>
2021-11-26 14:54:24 +0100

    fix configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('mssql_collector');
+        $treeBuilder = new TreeBuilder('mssql_collector');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
